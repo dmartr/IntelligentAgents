61c50a5ec8bea090505c971601f018d3321d4537
Ignacio Aguado <nachoaguadoc@gmail.com>
2016-10-24 00:04:01 +0200

    Finished code, lacking comments

--- workspace/deliberative/src/template/DeliberativeTemplate.java
+++ workspace/deliberative/src/template/DeliberativeTemplate.java
@@ -5,6 +5,8 @@ import logist.simulation.Vehicle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import logist.agent.Agent;
 import logist.behavior.DeliberativeBehavior;
@@ -40,7 +42,7 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 		this.topology = topology;
 		this.td = td;
 		this.agent = agent;
-		this.algorithm = Algorithm.BFS;
+		this.algorithm = Algorithm.ASTAR;
 		// initialize the planner
 		int capacity = agent.vehicles().get(0).capacity();
 		String algorithmName = agent.readProperty("algorithm", String.class, "ASTAR");
@@ -48,7 +50,7 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 		// Throws IllegalArgumentException if algorithm is unknown
 		algorithm = Algorithm.valueOf(algorithmName.toUpperCase());
 		
-		// ...
+
 	}
 	
 	@Override
@@ -58,16 +60,49 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 		// Compute the plan with the selected algorithm.
 		switch (algorithm) {
 		case ASTAR:
-			// ...
-			plan = naivePlan(vehicle, tasks);
+			System.out.println("ASTAR");
+			plan = aStarPlan(vehicle, tasks);
 			break;
 		case BFS:
-			// ...
 			plan = bfsPlan(vehicle, tasks);
 			break;
 		default:
 			throw new AssertionError("Should not happen.");
 		}		
+		return plan;
+	}
+	
+	private Plan aStarPlan(Vehicle vehicle, TaskSet tasks) {
+		City current = vehicle.getCurrentCity();
+		int capacity = vehicle.capacity();
+		int copyToDelete = vehicle.capacity();
+		int costPerKm = vehicle.costPerKm();
+		double benefits = 0.0;
+		double totalBenefits = 0.0;
+		double totalDistance = 0.0;
+		List<Task> toDeliverList = new ArrayList(vehicle.getCurrentTasks());
+		Plan plan = new Plan(current);
+		List<Task> toPickupList = new ArrayList(tasks);
+		List<DeliberativeAction> actionHistory = new ArrayList<DeliberativeAction>();
+		DeliberativeState initialState = new DeliberativeState(current, capacity, costPerKm, totalDistance, benefits, totalBenefits, toPickupList, toDeliverList, actionHistory, null);
+		AStar aStar = new AStar();
+		List<DeliberativeAction> actionPlanList = aStar.search(initialState, agent).actionHistory;
+		
+		for (DeliberativeAction action : actionPlanList) {
+			if (action.move) {
+				System.out.println(action.id);
+				plan.appendMove(action.nextCity);
+			} else if (action.pickup) {
+				copyToDelete = copyToDelete-action.pickedupTask.weight;
+				System.out.println("PICK UP in " + action.pickedupTask.pickupCity + " to " + action.pickedupTask.deliveryCity);
+				plan.appendPickup(action.pickedupTask);
+			} else {
+				System.out.println("DELIVERY in " + action.deliveredTask.deliveryCity);
+				copyToDelete = copyToDelete+action.deliveredTask.weight;
+				plan.appendDelivery(action.deliveredTask);
+			}
+		}
+		
 		return plan;
 	}
 	private Plan bfsPlan(Vehicle vehicle, TaskSet tasks) {
@@ -77,12 +112,14 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 		int costPerKm = vehicle.costPerKm();
 		double benefits = 0.0;
 		double totalBenefits = 0.0;
+		double totalDistance = 0.0;
+
 		List<Task> toDeliverList = new ArrayList(vehicle.getCurrentTasks());
 		Plan plan = new Plan(current);
 		List<Task> toPickupList = new ArrayList(tasks);
 		List<DeliberativeAction> actionHistory = new ArrayList<DeliberativeAction>();
-		DeliberativeState initialState = new DeliberativeState(current, capacity, costPerKm, benefits, totalBenefits, toPickupList, toDeliverList, actionHistory, null);
-		BreadthFirstSearch bfs = new BreadthFirstSearch();
+		DeliberativeState initialState = new DeliberativeState(current, capacity, costPerKm, totalDistance, benefits, totalBenefits, toPickupList, toDeliverList, actionHistory, null);
+		BFS bfs = new BFS();
 		List<DeliberativeAction> actionPlanList = bfs.search(initialState).actionHistory;
 		
 		for (DeliberativeAction action : actionPlanList) {
