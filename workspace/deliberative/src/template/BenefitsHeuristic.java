42a2f4376fb24e3cbde24c2114bc671832f01eac
Ignacio Aguado <nachoaguadoc@gmail.com>
2016-10-25 21:56:11 +0200

    Benefits heuristic

--- workspace/deliberative/src/template/BenefitsHeuristic.java
+++ workspace/deliberative/src/template/BenefitsHeuristic.java
@@ -7,22 +7,19 @@ package template;
 public class BenefitsHeuristic implements Heuristic{
 	
 	// g(s) = g(parent) + cost(s)
+	// Equivalent to the total cost accumulated for this state (cost - rewards)
 	// cost(s) is the opposite of the Benefit function defined in the State: the smaller the number, the better (less costs, more benefits)
 	public double getG(DeliberativeState state) {
-		double g = state.benefits;
-		if (state.parent != null) {
-			g += getG(state.parent);
-		}
-		return -g;
-	}
-	
-	// h(s): accumulated benefits from the initial state
-	public double getH(DeliberativeState state) {
 		return -state.totalBenefits;
 	}
 	
+	// h(s): minimum cost - rewards or remaining tasks
+	public double getH(DeliberativeState state) {
+		return state.getMinimumFutureDistance()*state.costPerKm-state.getMaxRewards();
+	}
+	
 	// f(s) = g(s) + h(s)
-	public double getF(DeliberativeState state) {	
+	public double getF(DeliberativeState state) {
 		return getG(state) + getH(state);
 	}
 
